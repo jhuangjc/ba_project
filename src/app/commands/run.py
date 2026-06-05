e9dd45190204569ab59f61ecdfec8940de7e194d
Jonas Huang <jonas.huang@gmx.de>
2026-06-05 12:25:45 +0200

    neue ordnerstruktur hab command geloescht und in pipeline verschoben generate und open sind depreacted,run ist jetzt die einzige funktion in commands
     Please enter the commit message for your changes. Lines starting

--- src/app/commands/run.py
+++ src/app/commands/run.py
@@ -1,21 +1,32 @@
 import json
 from app.utils.io import PROJECT_ROOT, read_file, load_input_files
+from app.pipeline.triple_generator import gen_triples
+
 REGISTRY_PATH = PROJECT_ROOT / "data/registry/registry.json"
+
 def run(args):
     ex_id = args.expId
     registry_data = json.loads(read_file(REGISTRY_PATH))
-    experiments =registry_data["experiments"]
+    experiments = registry_data["experiments"]
     exp = None
     for e in experiments:
-
-        if (e["experiment_id"] == ex_id):
-            exp=e
+        if e["experiment_id"] == ex_id:
+            exp = e
             break
     if exp is None:
-        raise ValueError( f"{ex_id} nicht in der Registry")
-    # lade den input test 
-    input_files = load_input_files(exp)
+        raise ValueError(f"{ex_id} nicht in der Registry")
+
+    # lade den input text
+    input_text = load_input_files(exp)
 
     # generiere die Tripel
+    triples_result = gen_triples(input_text)
 
-    #checkpoint
+    return {
+        "experiment_id": ex_id,
+        "input_type": exp["input_type"],
+        "source_group": exp["source_group"],
+        "gold_id": exp["gold_id"],
+        "entities": triples_result["entities"],
+        "triples": triples_result["triples"],
+    }
