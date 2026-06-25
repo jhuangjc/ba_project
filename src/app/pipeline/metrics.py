2b2264ecdfb7c90aeaca4966cdd0ee4a6900b7bb
Jonas Huang <jonas.huang@gmx.de>
2026-06-25 12:34:29 +0200

    add skeleton code for metrics implementation

---
+++ src/app/pipeline/metrics.py
@@ -0,0 +1,26 @@
+#####################utility functions fuer metriken#####################
+def calculate_precision(predicted, goldstandard):
+    pass
+def calculate_recall(predicted, goldstandard):
+    pass
+def calculate_f1(predicted, goldstandard):
+    pass
+
+
+
+#hilfsfunktion um die predicted triples mit den goldstandard triples zu vergleichen, wichtig fuer das endergebnis
+def map_to_gold(entities,goldstandard):
+
+
+    pass
+#####################orchastration functions fuer metriken#####################
+#orchastration fucntion for the measurement before and after refinement,
+def measure_data(predicted,goldstandard, before_refinement):
+
+    metrics = {}
+
+    
+    return metrics
+#orchastration fucntion to generate the combined metrics to complete the set of metrics
+def generate_combined_metrics(metrics_before, metrics_after, goldstandard, refined_result):
+    pass
